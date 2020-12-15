37d6ebfab71b92ff0a09f4611607757858091bc1
crohn44 <notmeae@gmail.com>
2020-12-15 12:24:27 -0500

    initial commit

---
+++ oop_excersizes.py
@@ -0,0 +1,17 @@
+class Person:
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+
+    def greet(self, other_person):
+        print('Hello {}, I am {}!'.format(other_person.name, self.name))
+
+    def print_contact_info(self):
+        print("{}'s email: {}\n{}'s phone: {}".format(self.name, self.email, self.name, self.phone))
+
+sonny = Person('sonny', 'sonny@hotmail.com', '483-485-4948')
+jordan = Person('jordan', 'jordan@aol.com', '495-586-3456')
+
+sonny.greet(jordan)
+jordan.greet(sonny)
