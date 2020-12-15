0f3e8a300721088cf5a21cad09aa0c1e3dbfd876
crohn44 <notmeae@gmail.com>
2020-12-15 12:40:10 -0500

    added add_friend and num_friends methods to Person class

--- oop_excersizes.py
+++ oop_excersizes.py
@@ -10,6 +10,13 @@ class Person:
 
     def print_contact_info(self):
         print("{}'s email: {}\n{}'s phone: {}".format(self.name, self.email, self.name, self.phone))
+    
+    def add_friend(self, friend):
+        self.friends.append(friend)
+    
+    def num_friends(self):
+        print(len(self.friends))
+
 
 sonny = Person('sonny', 'sonny@hotmail.com', '483-485-4948')
 jordan = Person('jordan', 'jordan@aol.com', '495-586-3456')
@@ -17,7 +24,5 @@ jordan = Person('jordan', 'jordan@aol.com', '495-586-3456')
 sonny.greet(jordan)
 jordan.greet(sonny)
 sonny.print_contact_info()
-jordan.friends.append(sonny)
-sonny.friends.append(jordan)
-
-print(len(jordan.friends))
+jordan.add_friend(sonny)
+jordan.num_friends()
